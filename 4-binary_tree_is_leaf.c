a42146345425d09bd0af7d4f62eafd4b7b0b6a6e
root <root@avellin.localdomain>
2024-01-04 15:17:05 +0200

    updates

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - function that checks if node is leaf
+ * @node: pointer to the node to check
+ * Return: 1 if leaf else 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->left != NULL || node->right != NULL)
+		return (0);
+	else
+		return (1);
+}
