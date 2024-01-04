32a494d3278ad0d136d78128790dec3b0e6ed14f
root <root@avellin.localdomain>
2024-01-04 10:46:02 +0200

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -24,7 +24,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left == NULL)
 		parent->left = new;
 
-	if (parent->left != NULL)
+	else
 	{
 		new->left = parent->left;
 		parent->left = new;
