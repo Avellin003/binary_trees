ce99f89f3b8c2e2d71c21b2c95902587904f020c
root <root@avellin.localdomain>
2024-01-04 10:35:52 +0200

    updates

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,6 +7,7 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new = NULL;
 	binary_tree_t *new = malloc(sizeof(binary_tree_t));
 
 	if (parent == NULL)
@@ -25,7 +26,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left = new;
 		new->left->parent = new;
 	}
-	parent->left = new;
+	else
+		parent->left = new;
 
 	return (new);
 }
